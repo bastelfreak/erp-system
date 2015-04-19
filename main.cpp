d8671ad33a168fc7ae831601788554a8fa9c2c80
Tim Meusel <tim@bastelfreak.de>
2015-04-19 19:47:05 +0200

    add basic main function

--- main.cpp
+++ main.cpp
@@ -3,14 +3,29 @@ Created by Meusel
 Responsible: Meusel?
 handles all stuff
 */
+
+// include global stuff
+#include <iostream>
+#include <string>
+
+// include our own stuff
 #include "articlesearch.h"
 #include "filehandler.h"
 #include "menue.h"
 #include "statistics.h"
-#include "tarticle"
+#include "tarticle.h"
 
 
 void main (void)
-{
+{	
+	string db_path = "/path/to/file";
+	bool open_database = false;
+	open_database = open(db_path);
 
+	if (open_database != true)
+	{
+		std::cout << "We were unable to open a valid database, we tried" << db_path << "\n";
+	} else {
+		// do the awesome stuff
+	}
 }
