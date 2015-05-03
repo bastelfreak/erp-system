8218c8eb0e173b7fb73e97012068e64fb858cfd5
StoneCat <StoneCat@luis.is>
2015-05-03 21:35:22 +0200

    Anpassung Main
    Anbei ein Vorschlag für die Main nach meinen Änderungen

--- main.cpp
+++ main.cpp
@@ -16,29 +16,31 @@ handles all stuff
 
 
 int main (void)
-{	
+{
+	TArticle Artikel[1000];
 	std::string db_path = "database.csv";
 	bool exit = false;
 	char input;
-  std::fstream *database = open(db_path);
-  getLine(database);
-  while(!exit)
-  {
-    int methodNumber = menue();
-    switch(methodNumber)
-    {
-      case 1: break;
-      case 2: break;
-      case 3: break;
-      case 4: break;
-      default: break;
-    }
-    std::cout << "Do you want to exit the Tool(y)?" << "\n";
-    std::cin >> input;
-    if(input == 'y' || input == 'Y' || input == 'z' || input == 'Z')
-    {
-      exit = true;
-    }
-  }
+	std::fstream database;
+	open(db_path, database);
+	getData(database, Artikel);
+	while(!exit)
+	{
+		int methodNumber = menue();
+		switch(methodNumber)
+		{
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			default: break;
+		}
+		std::cout << "Do you want to exit the Tool(y)?" << "\n";
+		std::cin >> input;
+		if(input == 'y' || input == 'Y' || input == 'z' || input == 'Z')
+		{
+			exit = true;
+		}
+	}
   return 0;
 }
