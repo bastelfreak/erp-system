53f725d0f06bc122ec2c5a1528d0a7b6e05e92bf
Tim 'bastelfreak' Meusel <tim@bastelfreak.de>
2015-05-04 21:10:14 +0200

    martin said I should remove that

--- statistics.h
+++ statistics.h
@@ -11,4 +11,3 @@ provides functions for statistics
 int teuersterArtikel(TArticle ZuSuchendeArtikel[]);		// Findet den teuersten Artikel
 int guenstigsterArtikel(TArticle ZuSuchendeArtikel[]);	// Findet den günstigsten Artikel
 int anzahlArtikel(TArticle ZuSuchendeArtikel[]);		// Findet die Anzahl der Artikel
-int durschschnittArtikel(TArticle ZuSuchendeArtikel[]);	// Errechnet den Durschnittspreis aller Artikel
